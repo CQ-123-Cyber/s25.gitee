d14a6254cbabc1e56d97eb19f0b108fa7a3f485f
wupeiqi <wupeiqi@live.com>
2020-03-16 11:15:51 +0800

    母版准备

---
+++ web/views/account.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+用户账户相关功能：注册、短信、登录、注销
+"""
+from django.shortcuts import render
+
+
+def register(request):
+    return render(request, 'register.html')

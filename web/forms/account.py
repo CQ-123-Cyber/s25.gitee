26b3144c3e3fc5f24e79c278e080f38c1b4fbfde
wupeiqi <wupeiqi@live.com>
2020-03-16 16:13:05 +0800

    手机格式&短信模板 校验

--- web/forms/account.py
+++ web/forms/account.py
@@ -3,12 +3,12 @@
 from django import forms
 from django.core.validators import RegexValidator
 from django.core.exceptions import ValidationError
+from django.conf import settings
 
 from web import models
 
 
 class RegisterModelForm(forms.ModelForm):
-
     mobile_phone = forms.CharField(label='手机号', validators=[RegexValidator(r'^(1[3|4|5|6|7|8|9])\d{9}$', '手机号格式错误'), ])
 
     password = forms.CharField(
@@ -31,3 +31,28 @@ class RegisterModelForm(forms.ModelForm):
         for name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
             field.widget.attrs['placeholder'] = '请输入%s' % (field.label,)
+
+
+class SendSmsForm(forms.Form):
+    mobile_phone = forms.CharField(label='手机号', validators=[RegexValidator(r'^(1[3|4|5|6|7|8|9])\d{9}$', '手机号格式错误'), ])
+
+    def __init__(self, request, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.request = request
+
+    def clean_mobile_phone(self):
+        """ 手机号校验的钩子 """
+        mobile_phone = self.cleaned_data['mobile_phone']
+
+        # 判断短信模板是否有问题
+        tpl = self.request.GET.get('tpl')
+        template_id = settings.TENCENT_SMS_TEMPLATE.get(tpl)
+        if not template_id:
+            raise ValidationError('短信模板错误')
+
+        # 校验数据库中是否已有手机号
+        exists = models.UserInfo.objects.filter(mobile_phone=mobile_phone).exists()
+        if exists:
+            raise ValidationError('手机号已存在')
+
+        return mobile_phone
--- web/views/account.py
+++ web/views/account.py
@@ -4,7 +4,9 @@
 用户账户相关功能：注册、短信、登录、注销
 """
 from django.shortcuts import render, HttpResponse
-from web.forms.account import RegisterModelForm
+from web.forms.account import RegisterModelForm, SendSmsForm
+
+
 
 
 def register(request):
@@ -15,6 +17,8 @@ def register(request):
 
 def send_sms(request):
     """ 发送短信 """
-    print(request.GET)
-
+    form = SendSmsForm(request, data=request.GET)
+    # 只是校验手机号：不能为空、格式是否正确
+    if form.is_valid():
+        pass
     return HttpResponse('成功')

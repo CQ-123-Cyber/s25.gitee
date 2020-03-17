80f1bc310bf0b2492f7f80ba4661c29e240086e9
wupeiqi <wupeiqi@live.com>
2020-03-17 10:10:47 +0800

    用户注册默认完成 1. 短信获取验证码 2. reids校验和注册用户

--- web/views/account.py
+++ web/views/account.py
@@ -6,12 +6,22 @@
 from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
 from web.forms.account import RegisterModelForm, SendSmsForm
+from web import models
 
 
 def register(request):
     """ 注册 """
-    form = RegisterModelForm()
-    return render(request, 'register.html', {'form': form})
+    if request.method == 'GET':
+        form = RegisterModelForm()
+        return render(request, 'register.html', {'form': form})
+
+    form = RegisterModelForm(data=request.POST)
+    if form.is_valid():
+        # 验证通过，写入数据库（密码要是密文）
+        form.save()
+        return JsonResponse({'status': True, 'data': '/login/'})
+
+    return JsonResponse({'status': False, 'error': form.errors})
 
 
 def send_sms(request):

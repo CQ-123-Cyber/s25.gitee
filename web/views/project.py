c87e8a89125b5aad5ed7d8d2fff2e2ed3ba6e9c8
wupeiqi <wupeiqi@live.com>
2020-03-20 09:34:41 +0800

    项目列表展示

--- web/views/project.py
+++ web/views/project.py
@@ -5,13 +5,42 @@ from django.http import JsonResponse
 
 from web.forms.project import ProjectModelForm
 
+from web import models
+
 
 def project_list(request):
     """ 项目列表 """
     if request.method == "GET":
-        form = ProjectModelForm(request)
-        return render(request, 'project_list.html', {'form': form})
+        # GET请求查看项目列表
+        """
+        1. 从数据库中获取两部分数据
+            我创建的所有项目：已星标、未星标
+            我参与的所有项目：已星标、未星标
+        2. 提取已星标
+            列表 = 循环 [我创建的所有项目] + [我参与的所有项目] 把已星标的数据提取
+        
+        得到三个列表：星标、创建、参与
+        """
+        project_dict = {'star': [], 'my': [], 'join': []}
 
+        my_project_list = models.Project.objects.filter(creator=request.tracer.user)
+        for row in my_project_list:
+            if row.star:
+                project_dict['star'].append(row)
+            else:
+                project_dict['my'].append(row)
+
+        join_project_list = models.ProjectUser.objects.filter(user=request.tracer.user)
+        for item in join_project_list:
+            if item.star:
+                project_dict['star'].append(item.project)
+            else:
+                project_dict['join'].append(item.project)
+
+        form = ProjectModelForm(request)
+        return render(request, 'project_list.html', {'form': form, 'project_dict': project_dict})
+
+    # POST，对话框的ajax添加项目。
     form = ProjectModelForm(request, data=request.POST)
     if form.is_valid():
         # 验证通过：项目名、颜色、描述 + creator谁创建的项目？

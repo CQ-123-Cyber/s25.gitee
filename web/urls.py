736bda23682dd3485978ef7d75b2ab7b9078fc2c
wupeiqi <wupeiqi@live.com>
2020-03-23 12:26:17 +0800

    预览文章

--- web/urls.py
+++ web/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
 
         url(r'^wiki/$', wiki.wiki, name='wiki'),
         url(r'^wiki/add/$', wiki.wiki_add, name='wiki_add'),
+        url(r'^wiki/catalog/$', wiki.wiki_catalog, name='wiki_catalog'),
 
         url(r'^setting/$', manage.setting, name='setting'),
     ], None, None)),

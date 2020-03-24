fc76335635c31b05a782bd8b59faec05d45e461d
wupeiqi <wupeiqi@live.com>
2020-03-24 09:40:15 +0800

    wiki基本管理：markdown编辑器

--- web/views/wiki.py
+++ web/views/wiki.py
@@ -13,16 +13,16 @@ def wiki(request, project_id):
     if not wiki_id or not wiki_id.isdecimal():
         return render(request, 'wiki.html')
 
-    wiki_object = models.Wiki.objects.filter(id=wiki_id,project_id=project_id).first()
+    wiki_object = models.Wiki.objects.filter(id=wiki_id, project_id=project_id).first()
 
-    return render(request, 'wiki.html',{'wiki_object':wiki_object})
+    return render(request, 'wiki.html', {'wiki_object': wiki_object})
 
 
 def wiki_add(request, project_id):
     """ wiki添加 """
     if request.method == 'GET':
         form = WikiModelForm(request)
-        return render(request, 'wiki_add.html', {'form': form})
+        return render(request, 'wiki_form.html', {'form': form})
     form = WikiModelForm(request, data=request.POST)
     if form.is_valid():
         # 判断用户是否已经选择父文章
@@ -35,7 +35,7 @@ def wiki_add(request, project_id):
         url = reverse('wiki', kwargs={'project_id': project_id})
         return redirect(url)
 
-    return render(request, 'wiki_add.html', {'form': form})
+    return render(request, 'wiki_form.html', {'form': form})
 
 
 def wiki_catalog(request, project_id):
@@ -47,3 +47,36 @@ def wiki_catalog(request, project_id):
         'depth', 'id')
     # data = models.Wiki.objects.filter(project=request.tracer.project).values("id", 'title', 'parent_id')
     return JsonResponse({'status': True, 'data': list(data)})
+
+
+def wiki_delete(request, project_id, wiki_id):
+    """ 删除文章 """
+
+    models.Wiki.objects.filter(project_id=project_id, id=wiki_id).delete()
+
+    url = reverse('wiki', kwargs={'project_id': project_id})
+    return redirect(url)
+
+
+def wiki_edit(request, project_id, wiki_id):
+    """ 编辑文章 """
+    wiki_object = models.Wiki.objects.filter(project_id=project_id, id=wiki_id).first()
+    if not wiki_object:
+        url = reverse('wiki', kwargs={'project_id': project_id})
+        return redirect(url)
+    if request.method == "GET":
+        form = WikiModelForm(request, instance=wiki_object)
+        return render(request, 'wiki_form.html', {'form': form})
+
+    form = WikiModelForm(request, data=request.POST, instance=wiki_object)
+    if form.is_valid():
+        if form.instance.parent:
+            form.instance.depth = form.instance.parent.depth + 1
+        else:
+            form.instance.depth = 1
+        form.save()
+        url = reverse('wiki', kwargs={'project_id': project_id})
+        preview_url = "{0}?wiki_id={1}".format(url, wiki_id)
+        return redirect(preview_url)
+
+    return render(request, 'wiki_form.html', {'form': form})

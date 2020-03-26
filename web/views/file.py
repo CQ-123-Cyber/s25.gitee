967c6853d99b6cb201965756d5c832de925fa002
wupeiqi <wupeiqi@live.com>
2020-03-26 09:50:04 +0800

    文件管理-创建文件夹

---
+++ web/views/file.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from django.shortcuts import render
+from django.http import JsonResponse
+from web.forms.file import FolderModelForm
+from web import models
+
+
+# http://127.0.0.1:8002/manage/1/file/
+# http://127.0.0.1:8002/manage/1/file/?folder=1
+def file(request, project_id):
+    """ 文件列表 & 添加文件夹 """
+
+    parent_object = None
+    folder_id = request.GET.get('folder', "")
+    if folder_id.isdecimal():
+        parent_object = models.FileRepository.objects.filter(id=int(folder_id), file_type=2,
+                                                             project=request.tracer.project).first()
+
+    if request.method == "GET":
+        form = FolderModelForm(request, parent_object)
+        return render(request, 'file.html', {'form': form})
+
+    # 添加文件件
+    form = FolderModelForm(request, parent_object, data=request.POST)
+    if form.is_valid():
+        form.instance.project = request.tracer.project
+        form.instance.file_type = 2
+        form.instance.update_user = request.tracer.user
+        form.instance.parent = parent_object
+        form.save()
+        return JsonResponse({'status': True})
+
+    return JsonResponse({'status': False, 'error': form.errors})

e46433dc3548c9440ef23b41c7a3522efa49daf0
wupeiqi <wupeiqi@live.com>
2020-03-26 11:05:38 +0800

    文件管理-编辑文件夹

--- web/views/file.py
+++ web/views/file.py
@@ -2,6 +2,8 @@
 # -*- coding:utf-8 -*-
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.forms import model_to_dict
+
 from web.forms.file import FolderModelForm
 from web import models
 
@@ -17,12 +19,45 @@ def file(request, project_id):
         parent_object = models.FileRepository.objects.filter(id=int(folder_id), file_type=2,
                                                              project=request.tracer.project).first()
 
+    # GET 查看页面
     if request.method == "GET":
-        form = FolderModelForm(request, parent_object)
-        return render(request, 'file.html', {'form': form})
 
-    # 添加文件件
-    form = FolderModelForm(request, parent_object, data=request.POST)
+        breadcrumb_list = []
+        parent = parent_object
+        while parent:
+            # breadcrumb_list.insert(0, {'id': parent.id, 'name': parent.name})
+            breadcrumb_list.insert(0, model_to_dict(parent, ['id', 'name']))
+            parent = parent.parent
+
+        # 当前目录下所有的文件 & 文件夹获取到即可
+        queryset = models.FileRepository.objects.filter(project=request.tracer.project)
+        if parent_object:
+            # 进入了某目录
+            file_object_list = queryset.filter(parent=parent_object).order_by('-file_type')
+        else:
+            # 根目录
+            file_object_list = queryset.filter(parent__isnull=True).order_by('-file_type')
+        form = FolderModelForm(request, parent_object)
+
+        context = {
+            'form': form,
+            "file_object_list": file_object_list,
+            "breadcrumb_list": breadcrumb_list,
+        }
+        return render(request, 'file.html', context)
+
+    # POST 添加文件夹 & 文件夹的修改
+    fid = request.POST.get('fid', '')
+    edit_object = None
+    if fid.isdecimal():
+        edit_object = models.FileRepository.objects.filter(id=int(fid), file_type=2,
+                                                           project=request.tracer.project).first()
+
+    if edit_object:
+        form = FolderModelForm(request, parent_object, data=request.POST, instance=edit_object)
+    else:
+        form = FolderModelForm(request, parent_object, data=request.POST)
+
     if form.is_valid():
         form.instance.project = request.tracer.project
         form.instance.file_type = 2

aae0fe79f28a41ddc033a8bbb2feb71c2863571c
wupeiqi <wupeiqi@live.com>
2020-03-27 13:40:25 +0800

    上传文件-成功入库并在页面动态添加

--- web/views/file.py
+++ web/views/file.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+import json
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.forms import model_to_dict
+from django.views.decorators.csrf import csrf_exempt
 
-from web.forms.file import FolderModelForm
+from web.forms.file import FolderModelForm, FileModelForm
 from web import models
 
-from utils.tencent.cos import delete_file, delete_file_list
+from utils.tencent.cos import delete_file, delete_file_list, credential
 
 
 # http://127.0.0.1:8002/manage/1/file/
@@ -45,6 +47,7 @@ def file(request, project_id):
             'form': form,
             "file_object_list": file_object_list,
             "breadcrumb_list": breadcrumb_list,
+            'folder_object': parent_object
         }
         return render(request, 'file.html', context)
 
@@ -125,3 +128,80 @@ def file_delete(request, project_id):
     # 删除数据库中的文件
     delete_object.delete()
     return JsonResponse({'status': True})
+
+
+@csrf_exempt
+def cos_credential(request, project_id):
+    """ 获取cos上传临时凭证 """
+    per_file_limit = request.tracer.price_policy.per_file_size * 1024 * 1024
+    total_file_limit = request.tracer.price_policy.project_space * 1024 * 1024 * 1024
+
+    total_size = 0
+    file_list = json.loads(request.body.decode('utf-8'))
+    for item in file_list:
+        # 文件的字节大小 item['size'] = B
+        # 单文件限制的大小 M
+        # 超出限制
+        if item['size'] > per_file_limit:
+            msg = "单文件超出限制（最大{}M），文件：{}，请升级套餐。".format(request.tracer.price_policy.per_file_size, item['name'])
+            return JsonResponse({'status': False, 'error': msg})
+        total_size += item['size']
+
+        # 做容量限制：单文件 & 总容量
+
+    # 总容量进行限制
+    # request.tracer.price_policy.project_space  # 项目的允许的空间
+    # request.tracer.project.use_space # 项目已使用的空间
+    if request.tracer.project.use_space + total_size > total_file_limit:
+        return JsonResponse({'status': False, 'error': "容量超过限制，请升级套餐。"})
+
+    data_dict = credential(request.tracer.project.bucket, request.tracer.project.region)
+    return JsonResponse({'status': True, 'data': data_dict})
+
+
+@csrf_exempt
+def file_post(request, project_id):
+    """ 已上传成功的文件写入到数据 """
+    """
+    name: fileName,
+    key: key,
+    file_size: fileSize,
+    parent: CURRENT_FOLDER_ID,
+    # etag: data.ETag,
+    file_path: data.Location
+    """
+
+    # 根据key再去cos获取文件Etag和"db7c0d83e50474f934fd4ddf059406e5"
+
+    print(request.POST)
+    # 把获取到的数据写入数据库即可
+    form = FileModelForm(request, data=request.POST)
+    if form.is_valid():
+
+
+        # 通过ModelForm.save存储到数据库中的数据返回的isntance对象，无法通过get_xx_display获取choice的中文
+        # form.instance.file_type = 1
+        # form.update_user = request.tracer.user
+        # instance = form.save() # 添加成功之后，获取到新添加的那个对象（instance.id,instance.name,instance.file_type,instace.get_file_type_display()
+
+        # 校验通过：数据写入到数据库
+        data_dict = form.cleaned_data
+        data_dict.pop('etag')
+        data_dict.update({'project': request.tracer.project, 'file_type': 1, 'update_user': request.tracer.user})
+        instance = models.FileRepository.objects.create(**data_dict)
+
+        # 项目的已使用空间：更新 (data_dict['file_size'])
+        request.tracer.project.use_space += data_dict['file_size']
+        request.tracer.project.save()
+
+        result = {
+            'id': instance.id,
+            'name': instance.name,
+            'file_size': instance.file_size,
+            'username': instance.update_user.username,
+            'datetime': instance.update_datetime.strftime("%Y年%m月%d日 %H:%M"),
+            # 'file_type': instance.get_file_type_display()
+        }
+        return JsonResponse({'status': True, 'data': result})
+
+    return JsonResponse({'status': False, 'data': "文件错误"})
